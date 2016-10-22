4852715e24414dc417e3e9eeb0f1fd63bb4a9c3e
James Reeve <reevejamesd@gmail.com>
2016-10-22 14:47:07 -0400

    working on heroku db trait-id association

--- modules/database.js
+++ modules/database.js
@@ -7,6 +7,7 @@ pg.defaults.ssl = true;
 
 
 var insertSnp = function(id, location, basepair) {
+    console.log('entered insertSnp')
     pg.connect(process.env.DATABASE_URL, function (err, client) {
         if (err) throw err;
         client.query('INSERT INTO Ids_Snps (idUser, location, basepair) VALUES ($1, $2, $3)', [id, location, basepair], function (err, result) {
@@ -24,6 +25,7 @@ var insertSnp = function(id, location, basepair) {
 }
 
 var insertSnps = function(id, geneticData) {
+    console.log('entered insertSnps');
     pg.connect(process.env.DATABASE_URL, function(err, client) {
         if (err) {
             throw err;
@@ -36,6 +38,7 @@ var insertSnps = function(id, geneticData) {
 };
 
 var insertTrait = function(trait) {
+    console.log('entered insertTrait');
     pg.connect(process.env.DATABASE_URL, function (err, client) {
         if (err) throw err;
         client.query('INSERT INTO Traits (trait) VALUES ($1)', [trait], function (err, result) {
@@ -53,6 +56,7 @@ var insertTrait = function(trait) {
 }
 
 var insertTraits = function(idUser, traits, callback) {
+    console.log('entered insertTraits');
     for (i = 0; i < traits.length; i++) {
         insertTrait(traits[i]);    
     }           
@@ -95,6 +99,7 @@ var associateUserTraits = function(id, traits) {
                 if (err) throw err;
 
                 else {
+                    console.log('User trait Ids:\n')
                     console.log(JSON.stringify(result.rows));
                 }
             });
@@ -108,7 +113,9 @@ exports.insertUser = function(userJson) {
     console.log(JSON.stringify(userJson));
     insertSnps(userJson.id, userJson.geneticData);
     insertTraits(userJson.traits);
-    associateUserTraits(userJson.id, userJson.traits);
+    setTimeout(function() {
+        associateUserTraits(userJson.id, userJson.traits);
+    }, 2000); // figure out a better way later
     // insertTraits(userJson.id, userJson.traits, function(finished) {
     //     if (finished) {
 
