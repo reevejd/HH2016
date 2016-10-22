3bb6d855f027affb1d24e8bcc7ddfab0c09a8825
Paul Ruales <paul-ruales@uiowa.edu>
2016-10-22 17:35:00 -0400

    cookies for twitter and 23andme code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,8 +1,7 @@
 var socket = io()
 
 
-var thisUser;
-var thisUserTwitterHandle;
+
 
 socket.on("whatever", function(data) {
     console.log('You got data from the server:')
@@ -11,18 +10,22 @@ socket.on("whatever", function(data) {
 
 $(document).ready(function() {
     if (window.location.href.includes('?code=')) {
+        var thisUser;
         thisUser = window.location.href.split('?code=')[1];
+        setCookie("thisUser", thisUser, 365);
     }
+    console.log(window.location.href);
     if (window.location.href.includes('?twitter_handle=%40')) {
+      var thisUserTwitterHandle;
       thisUserTwitterHandle = window.location.href.split('?twitter_handle=%40')[1];
+      console.log(thisUserTwitterHandle);
+      setCookie("thisUserTwitterHandle", thisUserTwitterHandle, 365);
     }
 
 
 // sending stuff back to the server:
     $("#clickButton").on("click", function() {
         console.log('button clicked');
-        console.log("Client: " + thisUser);
-        console.log("Client: " + thisUserTwitterHandle);
         $.ajax({
             method: "POST",
             url: "/test",
@@ -33,7 +36,46 @@ $(document).ready(function() {
         });
     })
 
+    $("#logButton").on("click", function() {
+        console.log('log clicked');
+        console.log("Client: " + getCookie("thisUser"));
+        console.log("Client: " + getCookie("thisUserTwitterHandle"));
+    })
+
+    $("#clearCookiesButton").on("click", function() {
+        console.log('clearCookies clicked');
+        document.cookie = "thisUser=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+        document.cookie = "thisUserTwitterHandle=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
+        console.log("Cleared: " + getCookie("thisUser"));
+        console.log("Cleared: " + getCookie("thisUserTwitterHandle"));
+    })
 });
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    if(getCookie(cname) == ""){
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    }
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+
 
 // either use ajax:
 
