27e6caea59a7ee9b3211de983d39ca83ce819e78
Paul Ruales <paul-ruales@uiowa.edu>
2016-10-22 00:03:48 -0400

    scope problem with post call

--- index.js
+++ index.js
@@ -40,8 +40,9 @@ app.get('/genometoken', function(req, res) {
             client_secret=fdc2dceabe85b0336e7bc99b5eb6a4c3 ,
             grant_type=authorization_code ,
             code=zzz ,
-            "redirect_uri=http://localhost:8080/genometoken" \
-            "scope=genomes"
+            scope=genomes ,
+            "redirect_uri=http://localhost:8080/genometoken"
+
         }
     }, function(error, response, body){
         if(error) {
