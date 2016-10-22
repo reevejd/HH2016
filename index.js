3650b4bb177dd80e35f4ecf9ce1a293d2d800260
James Reeve <reevejamesd@gmail.com>
2016-10-21 22:51:41 -0400

    updated with example for passing info client->server

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var request = require('request'); // for making API calls 
+var bodyParser = require('body-parser');
 
 // port = process.env.PORT for deploying on cloud host (need this for heroku anyway, 8080 for local testing
 
 // setting up express 4 server & socket.io
 var express = require('express');
 var app = express();
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server)
 
@@ -17,7 +21,7 @@ server.listen(port, function() {
 // static files are stored in the public folder
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req,res) {
+app.get('/', function(req, res) {
   res.render('index');
 });
 
@@ -37,3 +41,11 @@ setTimeout(function() {
         someNumbers: [1, 2, 3, 4]
     });
 }, 7000);
+
+app.post('/test', function(req, res) {
+    console.log('user clicked button');
+    //console.log(JSON.stringify(req));
+    console.log(JSON.stringify(req.body));
+    console.log(JSON.stringify(req.body.data1));
+    res.send({status: "Success"});
+})
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,5 +1,3 @@
-alert("test");
-
 var socket = io()
 
 
@@ -7,3 +5,27 @@ socket.on("whatever", function(data) {
     console.log('You got data from the server:')
     console.log(data);
 });
+
+$(document).ready(function() {
+
+
+// sending stuff back to the server:
+    $("#clickButton").on("click", function() {
+        console.log('button clicked');
+        $.ajax({
+            method: "POST",
+            url: "/test",
+            data: {
+                data1: "test1", 
+                data2: ["hi,", "test"]
+            }
+        });
+    })
+
+});
+
+// either use ajax:
+
+
+
+//
