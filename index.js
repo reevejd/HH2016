141be076f5bd891a3f31406e356330310cb80a68
James Reeve <reevejamesd@gmail.com>
2016-10-22 00:13:26 -0400

    formatting params

--- index.js
+++ index.js
@@ -38,12 +38,12 @@ app.get('/genometoken', function(req, res) {
         //Lets post the following key/values as form
         json: {
 
-            client_id=be256e46c1e76dd5e8c76197f9168bed ,
-            client_secret=fdc2dceabe85b0336e7bc99b5eb6a4c3 ,
-            grant_type=authorization_code ,
-            code=zzz ,
-            "redirect_uri=http://localhost:8080/genometoken"
-            "scope=genomes"
+            client_id: "be256e46c1e76dd5e8c76197f9168bed",
+            client_secret: "fdc2dceabe85b0336e7bc99b5eb6a4c3",
+            grant_type: "authorization_code" ,
+            code: "zzz" ,
+            redirect_uri: "http://localhost:8080/genometoken",
+            scope: "genomes"
         }
     }, function(error, response, body){
         if(error) {
