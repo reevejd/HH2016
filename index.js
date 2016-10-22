9ba1049147744fe1e547deedb91af0c6a7f40dd5
Paul Ruales <paul-ruales@uiowa.edu>
2016-10-21 23:59:37 -0400

    fixing post call

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/genometoken', function(req, res) {
             client_secret=fdc2dceabe85b0336e7bc99b5eb6a4c3 ,
             grant_type=authorization_code ,
             code=zzz ,
-            "redirect_uri=http://localhost:8080/genometoken" ,
+            "redirect_uri=http://localhost:8080/genometoken" 
             "scope=genomes"
         }
     }, function(error, response, body){
