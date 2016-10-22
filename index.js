3b78864526db26d6fdac3572d9a841aabe974e8d
James Reeve <reevejamesd@gmail.com>
2016-10-22 18:20:00 -0400

    changing testing params

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ app.post('/test', function(req, res) {
     //db.insertUser(info);
 
 
-    console.log(JSON.stringify(db.getAssociations(['likes to read', 'likes movies', 'likes nothing'], 'TraitstoDNA')));
+    console.log(JSON.stringify(db.getAssociations(['likes to read', 'likes nothing'], 'TraitstoDNA')));
 
     res.send({status: "Success"});
 });
