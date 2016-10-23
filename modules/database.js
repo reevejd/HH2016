83193e7408647aefffb872670d899da0710125af
James Reeve <reevejamesd@gmail.com>
2016-10-23 00:59:55 -0400

    trying new connection method

--- modules/database.js
+++ modules/database.js
@@ -4,54 +4,32 @@
 var pg = require("pg");
 pg.defaults.ssl = true;
 
+var parsedJSON
+
+var Pool = require('pg').Pool;
+var pool = new Pool(process.env.DATABASE_URL);
 
 
 var insertSnp = function(id, location, basepair) {
     console.log('entered insertSnp')
-    pg.connect(process.env.DATABASE_URL, function (err, client) {
-        if (err) throw err;
-        client.query('INSERT INTO Ids_Snps (idUser, location, basepair) VALUES ($1, $2, $3)', [id, location, basepair], function (err, result) {
+   
+        pool.query('INSERT INTO Ids_Snps (idUser, location, basepair) VALUES ($1, $2, $3)', [id, location, basepair], function (err, result) {
             if (err) console.log(err);
 
-            client.end(function (err) {
-                if (err) throw err;
-
-                else {
-                    console.log("query result " + JSON.stringify(result));
-                }
-            });
         });
-    });
-}
+    
+};
 
 var insertSnps = function(id, geneticData) {
-    console.log('entered insertSnps');
-    pg.connect(process.env.DATABASE_URL, function(err, client) {
-        if (err) {
-            throw err;
-        } else {
-            for (var key in geneticData) {
-                insertSnp(id, key, geneticData[key]);
-            }
-        }
-    })
+    for (var key in geneticData) {
+        insertSnp(id, key, geneticData[key]);
+    }   
 };
 
 var insertTrait = function(trait) {
     console.log('entered insertTrait');
-    pg.connect(process.env.DATABASE_URL, function (err, client) {
-        if (err) throw err;
-        client.query('INSERT INTO Traits (trait) VALUES ($1)', [trait], function (err, result) {
-            if (err) console.log(err);
-
-            client.end(function (err) {
-                if (err) throw err;
-
-                else {
-                    console.log(JSON.stringify(result));
-                }
-            });
-        });
+    pool.query('INSERT INTO Traits (trait) VALUES ($1)', [trait], function (err, result) {
+            
     });
 }
 
@@ -86,25 +64,15 @@ var associateUserTraits = function(id, traits) {
     traitList = traitList.substr(0, traitList.length -2);
     console.log(traitList);
 
-    pg.connect(process.env.DATABASE_URL, function (err, client) {
-        if (err) throw err;
-        client.query('SELECT idTrait FROM Traits WHERE trait IN (' + traitList + ')', function (err, result) {
-            if (err) console.log(err);
+    pool.query('SELECT idTrait FROM Traits WHERE trait IN (' + traitList + ')', function (err, result) {
+        if (err) console.log(err);
 
-            client.end(function (err) {
+            console.log('User trait Ids:\n')
+            console.log("query result " + JSON.stringify(result));
+            for (var i = 0; i < result.rows.length; i++ ) {
+                associateUserTrait(id, result.rows[i].idtrait);
 
-                if (err) throw err;
-
-                else if (result) {
-                    console.log('User trait Ids:\n')
-                    console.log("query result " + JSON.stringify(result));
-                    for (var i = 0; i < result.rows.length; i++ ) {
-                        associateUserTrait(id, result.rows[i].idtrait);
-
-                    }
-                }
-            });
-        });
+            }
     });
 }
 
