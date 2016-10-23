642fe1844496e25ba39e65bcb546a5bdd7a1a432
James Reeve <reevejamesd@gmail.com>
2016-10-23 04:48:43 -0400

    console readabliity

--- index.js
+++ index.js
@@ -394,7 +394,7 @@ function snpMaster(dnaCode, callback){
 }//end dnaMaster
 
 app.post('/send-only-genetics', function(req, res) {
-    console.log('user is sending client genetic data only');
+    console.log('\n \n user is sending client genetic data only \n\n');
     //console.log(JSON.stringify(req));
     console.log(JSON.stringify(req.body));
 
